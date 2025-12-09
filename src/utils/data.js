1044826bd05547dda9f8385099b7b937da572140
rpdouglas <rpdouglas@gmail.com>
2025-12-09 08:11:24 -0500

    Adjusted Jeopardy Formatting

--- src/utils/data.js
+++ src/utils/data.js
@@ -203,10 +203,10 @@ export const MEETING_LINKS = {
 
 export const APP_VERSIONS = {
     DASHBOARD: '1.5.0',
-    JOURNAL: '1.11.1', 
+    JOURNAL: '1.12.0', 
     GOALS: '1.5.0', 
     COPING: '2.2.0', 
-    WORKBOOK: '1.8.1',
+    WORKBOOK: '1.9.0',
     LITERATURE: '1.3.0',
     RESOURCES: '1.1.0',
     SETTINGS: '1.3.0',
